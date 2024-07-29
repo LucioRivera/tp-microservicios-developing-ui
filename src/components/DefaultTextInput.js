c1a8b7a9f8b14dc46279586f2169d195cad65f10
AfterJack <luciorivera2000@gmail.com>
2024-07-29 04:55:56 -0300

    Input queda bloqueado o desbloqueado

--- src/components/DefaultTextInput.js
+++ src/components/DefaultTextInput.js
@@ -1,10 +1,11 @@
 import './globals.css'
 
-export default function DefaultTextInput({label, id}) {
+export default function DefaultTextInput({label, id, placeholder, enabled=true}) {
+
   return(
     <div className="w-full box-border rounded-lg">
       <label for={id} className="mb-2 text-large font-medium text-gray-900">{label}</label>
-      <input type="text" id={id} className="bg-grey-50 border border-grey-300 text-grey-900 text-sm rounded-lg focus:ring-red-500 focus:boder-red-500 block w-full p-2.5"/>
+      <input type="text" id={id} placeholder={placeholder} className={`${!enabled ? 'bg-gray-200' : 'bg-gray-50'} border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:boder-red-500 block w-full p-2.5`} disabled={!enabled}/>
     </div>
   );
 }
