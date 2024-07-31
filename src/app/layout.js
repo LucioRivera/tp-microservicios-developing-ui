94b1b9a906555843e51d7e0247721ffb5920624a
AfterJack <luciorivera2000@gmail.com>
2024-07-31 06:07:39 -0300

    Docker configurado y funcionando

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import CircularButton from '../components/CircularButton'
-import Link from 'next/Link'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'Bienvenido!',
@@ -8,17 +8,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" class='h-full font-sans font-medium'>
+    <html lang="en" className='h-full font-sans font-medium'>
       <body>
-		<div class='bg-white border-gray-200 dark:bg-gray-900o h-6 block h-auto'> 
-			<div class='max-w flex flex-wrap items-center justify-around py-4 shadow-md'>
+		<div className='bg-white border-gray-200 dark:bg-gray-900o h-6 block h-auto'> 
+			<div className='max-w flex flex-wrap items-center justify-around py-4 shadow-md'>
 				<Link href='https://www.frsf.utn.edu.ar'>
-					<div class='flex items-center justify-around'>
-						<img class='h-12' src='https://seeklogo.com/images/U/universidad-nacional-tecnologica-utn-logo-3C9F14023F-seeklogo.com.png'/>
+					<div className='flex items-center justify-around'>
+						<img className='h-12' src='https://seeklogo.com/images/U/universidad-nacional-tecnologica-utn-logo-3C9F14023F-seeklogo.com.png'/>
 					</div>
 				</Link>
-				<div class='w-1/4 flex flex-nowrap item-center justify-around space-x-9'>
-					<CircularButton text='Pedidos'/>
+				<div className='w-1/4 flex flex-nowrap item-center justify-around space-x-9'>
+					<CircularButton text='PedidosXD'/>
 					<CircularButton text='Clientes'/>
 					<CircularButton text='Productos'/>
 				</div>
--- src/components/Clientes/Add/FormAddCliente.js
+++ src/components/Clientes/Add/FormAddCliente.js
@@ -5,12 +5,12 @@ export default function FormAddCliente() {
     return (
         <div>
             <form>
-                <div className="flex items-center justify-around space-x-12">
+                <div className="flex items-center justify-around space-x-20">
                     <DefaultTextInput label="Nombre" id="nombre"/>
                     <DefaultTextInput label="Email" id="nombre"/>
                 </div>
 
-                <div className="flex items-center justify-around space-x-12">
+                <div className="flex items-center justify-around space-x-20">
                     <DefaultTextInput label="CUIT" id="nombre"/>
                     <DefaultTextInput label="Maximo Descubierto" id="nombre"/>
                 </div>
